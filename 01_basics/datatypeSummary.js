9c0a5c8aac60ab78cb6211ca5e08572004a52535
Amey Tambe <fr3nZy.at@gmail.com>
2024-04-23 19:52:49 +0530

    memory type in js

--- 01_basics/datatypeSummary.js
+++ 01_basics/datatypeSummary.js
@@ -80,3 +80,38 @@ console.log(typeof myFunction)
 console.log(myFunction)
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Types of Memory - Stack and Heap
+
+// Primitive data type use Stack Memory  >>  Gives a copy of the value
+
+// NON Primitive data type use Heap Memory >> Gives the reference of the variable/object
+
+let myYoutubeName = "AmeyT"
+
+let anotherName = myYoutubeName
+
+anotherName = "TambeAA"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let userOne = {    
+    email:"at@gmail.com",
+    upi:"at@ybl"
+}
+
+// object is inside the curly braces.. userOne and userTwo are variables that hold the reference to the object declared inside the curly braces
+// the variables userOne and UserTwo will be stored on stack memory.. the object that these 2 variables are pointing to will be stored in heap
+
+let userTwo = userOne
+
+userTwo.email = "tambe@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
